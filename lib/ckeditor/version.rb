88dca4faf28c07cb8c7d7293e560a21c486c69d2
Gleb Tv <glebtv@gmail.com>
2016-08-08 13:53:49 +0300

    update to 4.5.10

--- lib/ckeditor/version.rb
+++ lib/ckeditor/version.rb
@@ -1,6 +1,6 @@
 module Ckeditor
   module Version
-    GEM = "4.5.7.2".freeze
-    EDITOR = "4.5.7".freeze
+    GEM = "4.5.10.1".freeze
+    EDITOR = "4.5.10".freeze
   end
 end
--- vendor/assets/javascripts/ckeditor/build-config.js
+++ vendor/assets/javascripts/ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/5ec07e4dce5a1f9720ce787a7b4cf717
+ * (2) http://ckeditor.com/builder/e1a706c3776db6a03e1f8ac40ecb43eb
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/5ec07e4dce5a1f9720ce787a7b4cf717
+ * (3) http://ckeditor.com/builder/download/e1a706c3776db6a03e1f8ac40ecb43eb
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
